1e20ea6ca25b44fa2528d1c6f2a80c4fcec68104
Tetiana Triukhan <t.triukhan@ukma.edu.ua>
2025-04-01 18:08:19 +0300

    Functions implemented

--- app/io/input.py
+++ app/io/input.py
@@ -1,14 +1,42 @@
+import pandas
 
 
 def read_from_console():
-    """Function to read from console."""
-    pass
+    """
+    This function reads input from console.
+
+    Returns:
+        (str): text read from console.
+    """
+    return input("Enter text: ")
 
 
 def read_from_file_using_build_in(file_path):
-    """Function to read from file using build in Python functions."""
-    pass
+    """
+    This function reads from file using build in Python functions.
+
+    Args:
+        file_path (str): path to file for reading.
+
+    Returns:
+        (str): text read from file.
+    """
+    with open(file_path, "r", encoding="utf-8") as file:
+        return file.read()
+
 
 def read_from_file_using_pandas(file_path):
-    """Function to read from file using Pandas."""
-    pass
+    """
+    This function reads from file using Pandas.
+
+    Args:
+        file_path (str): path to file for reading.
+
+    Returns:
+        (str): text read from file.
+    """
+    try:
+        file_content = pandas.read_csv(file_path)
+        return file_content.to_string()
+    except pandas.errors.EmptyDataError:
+        return ""
