84762cff95212912b9a584c349564770bc733410
Tetiana Triukhan <t.triukhan@ukma.edu.ua>
2025-04-01 18:08:19 +0300

    Functions implemented

--- app/io/output.py
+++ app/io/output.py
@@ -1,14 +1,30 @@
-
+import os
 
 def write_to_console(text):
-    """Function to write to console."""
-    pass
+    """
+    This function writes text to console.
+
+    Args:
+        text (str): Text to be written.
+
+    Returns:
+        None
+    """
+    print(text)
 
 
 def write_to_file_using_build_in(file_path, text):
-    """Function to write to file using build in Python functions"""
-    pass
+    """
+    This function writes text to file using build in Python functions.
+    If file does not exist, it creates it.
 
-def write_to_file_using_pandas(file_path, text):
-    """Function to write to file using pandas."""
-    pass
+    Args:
+        file_path (str): path to file for reading.
+        text (str): text to be written.
+
+    Returns:
+        None
+    """
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    with open(file_path, "w", encoding="utf-8") as file:
+        file.write(text)
--- app/main.py
+++ app/main.py
@@ -1,7 +1,24 @@
-
+from app.io.input import read_from_console, read_from_file_using_build_in, read_from_file_using_pandas
+from app.io.output import write_to_console, write_to_file_using_build_in
+import os
 
 def main():
-    pass
+    data_folder = "..\data"
+    input_txt_file_path = os.path.join(data_folder, "input.txt")
+    input_csv_file_path = os.path.join(data_folder, "input_data.txt")
+    output_file_path = os.path.join(data_folder, "output.txt")
+
+    text_console = read_from_console()
+    text_file = read_from_file_using_build_in(input_txt_file_path)
+    text_pandas = read_from_file_using_pandas(input_csv_file_path)
+    text_for_output = "\n\n".join([
+        f"Console Input:\n{text_console}",
+        f"File Content:\n{text_file}",
+        f"CSV Content:\n{text_pandas}"
+    ])
+
+    write_to_console(text_for_output)
+    write_to_file_using_build_in(output_file_path, text_for_output)
 
 
 if __name__ == "__main__":
