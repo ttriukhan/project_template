7977c249eaf4c2b14b6b54d2175e65550aa918ff
Tetiana Triukhan <t.triukhan@ukma.edu.ua>
2025-04-01 09:57:14 +0300

    Added stumps for functions of input and output

---
+++ app/io/input.py
@@ -0,0 +1,14 @@
+
+
+def read_from_console():
+    """Function to read from console."""
+    pass
+
+
+def read_from_file_using_build_in(file_path):
+    """Function to read from file using build in Python functions."""
+    pass
+
+def read_from_file_using_pandas(file_path):
+    """Function to read from file using Pandas."""
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,14 @@
+
+
+def write_to_console(text):
+    """Function to write to console."""
+    pass
+
+
+def write_to_file_using_build_in(file_path, text):
+    """Function to write to file using build in Python functions"""
+    pass
+
+def write_to_file_using_pandas(file_path, text):
+    """Function to write to file using pandas."""
+    pass
